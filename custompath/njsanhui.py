36a135e70999b8b0c783fa8da672062ac19de94e
prcseraph <328154026@qq.com>
2016-11-10 09:50:45 +0800

    * update server url * use md5 password cache

--- custompath/njsanhui.py
+++ custompath/njsanhui.py
@@ -21,7 +21,6 @@ def getValidCookie(cookie):
     return ";".join(list_)
 
 def logInSanhui(sh_usr, sh_pwd):
-    md5_password_ = getMd5Password(sh_usr, sh_pwd)
     # http://1565ek6742.iok.la:8088/njsanhui/XD.Login/token.ashx?method=get_token&token=null
     # &deviceid=32014066-AD8E-4518-B83F-87A9E690AEE2
     #
@@ -30,6 +29,9 @@ def logInSanhui(sh_usr, sh_pwd):
     #
     # {"LoginName": "13813948023", "MobileID": "c5581de51e5956c5c3f30a6f7d38efe3", "OS": "IOS",
     #  "MD5_Password": "6cd1b4683f9ea3ad7c3e3a370494d3b3", "DeviceID": "32014066-AD8E-4518-B83F-87A9E690AEE2"}
+    #
+    # md5_password_ = getMd5Password(sh_usr, sh_pwd)
+    md5_password_ = shconfig.gUsersDict[sh_usr]["md5_pwd"]
     url_ = shconfig.getInterfaceUrlSSL(sh_usr, "XD.Login/token.ashx", "get_token")
     params_ = {
         "LoginName": sh_usr,
@@ -109,6 +111,7 @@ def appEntry(sh_usr, sh_pwd="88888"):
         return False
 
 if __name__ == "__main__":
-    usr_ = "13813948023"
     pwd_ = "88888"
-    appEntry(usr_, pwd_)
+    for usr_ in shconfig.gUsersDict:
+        md5_pwd_ = getMd5Password(usr_, pwd_)
+        print usr_, md5_pwd_
--- custompath/shconfig.py
+++ custompath/shconfig.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 
-gHostUrl = "http://1565ek6742.iok.la:8088/njsanhui/AttachFiles/employeePhoto/"
-gServerUrl = "http://1565ek6742.iok.la:8088/njsanhui/"
-gServerUrlSSL = "http://1565ek6742.iok.la:8088/njsanhui/"
-gAttachUrl = "http://1565ek6742.iok.la:8088/njsanhui/"
-gVersion = "1.9.4"
-# "LoginName": "13813948023",
-# "MobileID": "6e110225688f68ee4708566ed971a27d",
-# "OS": "IOS",
-# "MD5_Password": "6cd1b4683f9ea3ad7c3e3a370494d3b3",
-# "DeviceID": "78265209-05AA-443F-A20B-502C902E63FF"
-# method=get_token&token=null&deviceid=78265209-05AA-443F-A20B-502C902E63FF
+gVersion = "2.0.1"
+# gHostUrl = "http://1565ek6742.iok.la:8088/njsanhui/AttachFiles/employeePhoto/"
+# gServerUrl = "http://1565ek6742.iok.la:8088/njsanhui/"
+# gServerUrlSSL = "http://1565ek6742.iok.la:8088/njsanhui/"
+# gAttachUrl = "http://1565ek6742.iok.la:8088/njsanhui/"
+gHostUrl = ' http://pic.haopan.net/employeePhoto/'
+gServerUrl = 'http://cloud.haopan.net/cloud/'
+gServerUrlSSL = 'http://cloud.haopan.net/cloud/'
+gAttachUrl = 'http://cloud.haopan.net/cloud/'
+
 gUsersDict = {
     "13952010386": {
         "rx_time": None,
         "my_name": "lq",
         "device_os": "IOS",
-        "device_id": "32014066-AD8E-4518-B83F-87A9E690AEE2",
+        "md5_pwd": "7ef5a27a887292981e8b7806405e5c9f",
         "mobile_id": "c5581de51e5956c5c3f30a6f7d38efe3",
+        "device_id": "32014066-AD8E-4518-B83F-87A9E690AEE2",
         "qiandao": 0,
         "response": {}
     },
@@ -25,8 +25,9 @@ gUsersDict = {
         "rx_time": None,
         "my_name": "jy",
         "device_os": "IOS",
-        "device_id": "78265209-05AA-443F-A20B-502C902E63FF",
+        "md5_pwd": "6cd1b4683f9ea3ad7c3e3a370494d3b3",
         "mobile_id": "6e110225688f68ee4708566ed971a27d",
+        "device_id": "78265209-05AA-443F-A20B-502C902E63FF",
         "qiandao": 0,
         "response": {}
     },
@@ -34,8 +35,9 @@ gUsersDict = {
         "rx_time": None,
         "my_name": "wzq",
         "device_os": "IOS",
-        "device_id": "32014066-AD8E-4518-B83F-87A9E690AEE2",
-        "mobile_id": "c5581de51e5956c5c3f30a6f7d38efe3",
+        "md5_pwd": "d8e6d32cdd8ee6af55e432dea3bcc161",
+        "mobile_id": "80cc4428e019be8d45bab8e19bc5811f",
+        "device_id": "9177AEDE-D67C-49E0-94BB-DE21B9AF90C7",
         "qiandao": 0,
         "response": {}
     },
@@ -43,8 +45,9 @@ gUsersDict = {
         "rx_time": None,
         "my_name": "llx",
         "device_os": "IOS",
-        "device_id": "32014066-AD8E-4518-B83F-87A9E690AEE2",
+        "md5_pwd": "e19a4ed4a9a0ff620f4eaff6d384f6c1",
         "mobile_id": "c5581de51e5956c5c3f30a6f7d38efe3",
+        "device_id": "32014066-AD8E-4518-B83F-87A9E690AEE2",
         "qiandao": 0,
         "response": {}
     }
